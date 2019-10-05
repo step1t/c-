813524be34f3381f1d612648b9e514c6f542cf08
nick <nick@tut.by>
2019-10-05 15:55:36 +0300

    конструктор котов

---
+++ ConsoleApp1/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cat
+    {
+        public string Name;
+        public float Age;
+        public BritEnum brit;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.Name);
+            sb.AppendLine(Age.ToString());
+            return sb.ToString();
+        }
+
+
+        public Cat(string name, float age, BritEnum brit = BritEnum.Brit)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.brit = brit;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My name is Tom");
+            Console.WriteLine(7.5);
+            Console.WriteLine(BritEnum.Exotic);
+
+
+            Cat cat1 = new Cat ("Vaska", 10.1f, BritEnum.Brit);
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat1.Age);
+            Console.WriteLine(cat1.brit);
+
+            Cat cat2 = new Cat("Barsik", 3);
+            Console.WriteLine(cat2.Name);
+            Console.WriteLine(cat2.Age);
+            Console.WriteLine(cat2.brit);
+
+            Cat cat3 = new Cat("Murzik", 4, BritEnum.None);
+            Console.WriteLine(cat3.Name);
+            Console.WriteLine(cat3.Age);
+            Console.WriteLine(cat3.brit);
+
+            Console.WriteLine(cat1);
+        }
+    }
+    
+}
